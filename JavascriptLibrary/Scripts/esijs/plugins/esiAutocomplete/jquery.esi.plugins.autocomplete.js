1d5fa324ac0325e596331c08787d4105c3f5597f
lyc-chengzi <305213781@qq.com>
2015-12-17 10:27:36 +0800

    自动提示框插件完成第一版

--- JavascriptLibrary/Scripts/esijs/plugins/esiAutocomplete/jquery.esi.plugins.autocomplete.js
+++ JavascriptLibrary/Scripts/esijs/plugins/esiAutocomplete/jquery.esi.plugins.autocomplete.js
@@ -34,7 +34,19 @@
                 var op = $.extend(ESIAutocomplete.defaultOptions, options || {});
                 var _self = this;
                 //给控件添加样式，并隐藏
-                jqueryElemet.addClass(this.attributes.className).width(op.width).height(op.height).hide();
+                jqueryElemet
+                    .append('<div class="content"></div>')
+                    .append('<div class="tools"><a class="close">关闭</a></div>')
+                    .addClass(this.attributes.className)
+                    .width(op.width)
+                    .height(op.height)
+                    .hide();
+                jqueryElemet.find("div.content").height(jqueryElemet.outerHeight() - 40);
+                jqueryElemet.find("div.tools a.close").on('click', function () {
+                    jqueryElemet.hide();
+                    return false;
+                });
+
                 //获得target对象(jquery对象)
                 this.$target = op.target;
                 if (!this.$target) {
@@ -42,14 +54,25 @@
                     return false;
                 }
                 var _targetOffset = this.$target.offset();
-                jqueryElemet.css("top", _targetOffset.top + this.$target.outerHeight()).css("left", _targetOffset.left);
-                //添加keydown事件
-                this.$target.on('keyup', function (e) {                    
-                    clearTimeout(_self.targetKeyDownIndex);
-                    //_self.targetKeyDownIndex = setTimeout(_self.targetEventHandler.call(_self), op.eventDelay)
-                    /*_self.targetKeyDownIndex = setTimeout(function () {
-                        alert(11111111111);
-                    }, 1000);*/
+                //设置提示框的位置偏移量
+                jqueryElemet
+                    .css("top", _targetOffset.top + this.$target.outerHeight() + op.pTop)
+                    .css("left", _targetOffset.left + op.pLeft);
+
+                //添加event事件
+                this.$target.on(op.eventType, function (e) {
+                    _self.targetKeyDownIndex && clearTimeout(_self.targetKeyDownIndex);
+                    if ($(this).val() == "") {
+                        jqueryElemet.find(".content").empty();
+                        return false;
+                    }
+                    _self.targetKeyDownIndex = setTimeout(__targetEventHandler, op.eventDelay, _self);
+                }).on('click', function (e) {
+                    //添加click事件
+                    if ($(this).val() != "") {
+                        jqueryElemet.show();
+                        return false;
+                    }
                 });
             });
         },
@@ -61,7 +84,7 @@
         Render: function (filterData) {
             for (var i = 0; i < filterData.length; i++) {
                 var _data = filterData[i];
-                this.jQueryObj.append('<div class="zb-info">' +
+                this.jQueryObj.find(".content").append('<div class="zb-info">' +
                                       '    <div class="title">' + _data.title + '</div>' +
                                       '    <div class="des">' + _data.fixDes + '</div>' +
                                       '</div>');
@@ -74,41 +97,7 @@
             } else {
                 return false;
             }
-        },
-        //获得过滤后的数据
-        getFilterData: function (text, callback) {
-            if (this.loadDataType !== 1 && this.loadDataType !== 2) {
-                console.warn('请先设置控件的loadDataType属性，1代表加载本地数据;2代表ajax加载远程数据');
-                return false;
-            }
-            if (this.loadDataType === 1 && !this.localData) {
-                console.warn('加载本地数据, 请先设置控件的localData属性');
-                return false;
-            }
-            if (this.loadDataType === 2 && !this.remoteURL) {
-                console.warn('ajax加载远程数据, 请先设置控件的remoteURL属性');
-                return false;
-            }
-
-            var filteredData = [];
-            //如果是加载本地数据
-            if (this.loadDataType === 1) {
-                for (var i = 0; i < this.localData.length; i++) {
-                    if (this.filterFunc(text, this.localData[i]) == true) {
-                        filteredData.push(this.localData[i]);
-                    }
-                }
-                this.jQueryObj.empty();
-                //调用回调函数渲染视图
-                callback.call(this, filteredData);
-            }
-            //如果是加载远程数据
-        },
-        //target对象事件处理函数
-        targetEventHandler: function () {
-            this.jQueryObj.show();
-            this.getFilterData(this.$target.val(), this.Render);
-        }
+        }        
     });
 
     ESIAutocomplete.prototype = subPlugins;
@@ -123,9 +112,52 @@
         //控件相对于target的left偏移量
         pLeft: 0,
         //事件延迟
-        eventDelay: 500
+        eventDelay: 500,
+        //事件类型
+        eventType: 'keyup'
     };
 
+
+    /*********************private function**********************/
+
+    //获得过滤后的数据
+    var __getFilterData = function (text, callback) {
+        if (this.loadDataType !== 1 && this.loadDataType !== 2) {
+            console.warn('请先设置控件的loadDataType属性，1代表加载本地数据;2代表ajax加载远程数据');
+            return false;
+        }
+        if (this.loadDataType === 1 && !this.localData) {
+            console.warn('加载本地数据, 请先设置控件的localData属性');
+            return false;
+        }
+        if (this.loadDataType === 2 && !this.remoteURL) {
+            console.warn('ajax加载远程数据, 请先设置控件的remoteURL属性');
+            return false;
+        }
+
+        var filteredData = [];
+        //如果是加载本地数据
+        if (this.loadDataType === 1) {
+            for (var i = 0; i < this.localData.length; i++) {
+                if (this.filterFunc(text, this.localData[i]) == true) {
+                    filteredData.push(this.localData[i]);
+                }
+            }
+            this.jQueryObj.find(".content").empty();
+            //调用回调函数渲染视图
+            callback.call(this, filteredData);
+        }
+            //如果是加载远程数据
+        else {
+
+        }
+    }
+
+    //target对象事件处理函数;_self参数为当前实例对象
+    var __targetEventHandler = function (_self) {
+        _self.jQueryObj.show();
+        __getFilterData.call(_self, _self.$target.val(), _self.Render);
+    }
     
 
     $esi.plugins.esiAutocomplete = ESIAutocomplete;
